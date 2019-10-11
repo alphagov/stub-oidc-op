eccd04f8cf23549b6a71ce8972547573d62ac3ed
James Howes <james.howes@digital.cabinet-office.gov.uk>
2019-10-11 13:55:05 +0100

    Only start redis if it isnt already running
    - Startup script check if redis is already running and if so, doesn't try to start it again
    - Add kill script to kill redis

---
+++ kill-service.sh
@@ -0,0 +1,2 @@
+docker container stop localredis
+docker container rm localredis
--- startup.sh
+++ startup.sh
@@ -7,6 +7,11 @@ cd "$(dirname "$0")"
 
 ./gradlew installDist
 
-docker run -d -p 6379:6379 --rm redis
+if [ ! $(docker top localredis > /dev/null 2>&1) ]
+then
+docker run --name localredis -d -p 6379:6379 --rm redis
+fi
+
+
 
 ./build/install/verify-stub-op/bin/verify-stub-op server $CONFIG_FILE
